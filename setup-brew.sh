6a09b20209cc2fe162a602342eef9ad4e0941f27
Oleg <infectedbyjs@gmail.com>
2024-09-18 23:38:53 +1000

    Update `yazi` config

--- setup-brew.sh
+++ setup-brew.sh
@@ -11,29 +11,35 @@ brew install node
 brew install git
 brew install lazygit
 brew install pnpm
-brew install yazi
 brew install fzf
 brew install nvim
 
+# Install Yazi & other tools
+brew install yazi
+brew install ffmpegthumbnailer  # for video thumbnails
+brew install sevenzip # for 7z
+brew install jq # for json
+brew install poppler # for pdf
+brew install fd # for file search
+brew install ripgrep # for file content search
+brew install fzf # for filetree navigation
+brew install zoxide # for historical navigation
+brew install imagemagick # for image preview
+
 # Install casks
-brew install 1password
+brew install --cask 1password
 brew install --cask wezterm
 brew install --cask cleanmymac
-brew install --cask docker
-brew install figma
-brew install firefox
-brew install firefox@developer-edition
-brew install forklift
-brew install google-chrome
-brew install google@chrome-canary
-brew install insomnia
-brew install logi-options+
-brew install raycast
-brew install safari-technology-preview
+brew install --cask firefox
+brew install --cask brave-browser
+brew install --cask insomnia
+brew install --cask logi-options+
+brew install --cask raycast
 brew install --cask telegram
-brew install visual-studio-code
+brew install --cask visual-studio-code
 
 # Install fonts
+brew install --cask font-symbols-only-nerd-font
 brew install --cask font-zed-mono-nerd-font
 brew install --cask font-jetbrains-mono
 brew install --cask font-fira-code-nerd-font
@@ -42,9 +48,6 @@ brew install --cask font-victor-mono
 brew install --cask font-commit-mono-nerd-font
 brew install --cask font-hack-nerd-font
 
-
-# Install tap formulas
-brew install joedrago/repo/avifenc
 
 # Remove outdated versions from the cellar.
 brew cleanup
