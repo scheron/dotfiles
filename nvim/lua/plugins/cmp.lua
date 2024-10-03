f5a497d5eb84a9bfd153deec695b7869b6d04426
Oleg <infectedbyjs@gmail.com>
2024-10-03 12:00:09 +1000

    Update nvim cmp plugin for proper suggestions

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,44 +1,109 @@
 return {
   "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-nvim-lua",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "onsails/lspkind.nvim",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "onsails/lspkind.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "supermaven-inc/supermaven-nvim",
+      event = "VimEnter",
+      config = function()
+        require("supermaven-nvim").setup {
+          disable_keymaps = true,
+        }
+      end,
+    },
+  },
   config = function()
     local cmp = require "cmp"
+    local devicons = require "nvim-web-devicons"
+    local supermaven = require "supermaven-nvim.completion_preview" 
+
+    local kind_icons = {
+      Text = "",
+      Method = "",
+      Function = "ƒ",
+      Constructor = "",
+      Field = "",
+      Variable = "",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      Reference = "",
+      File = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = "",
+      Supermaven = "",
+    }
 
     cmp.setup {
       mapping = {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.confirm { select = true }
+          elseif supermaven.has_suggestion() then
+            supermaven.on_accept_suggestion()
           else
             fallback()
           end
         end, { "i", "s" }),
 
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm { select = true },
+
+        ["<C-i>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if supermaven.has_suggestion() then
+              supermaven.on_accept_suggestion()
+              fallback()
+            else
+              fallback()
+            end
+          else
+            cmp.complete()
+          end
+        end, { "i", "s" }),
       },
+
       sources = cmp.config.sources {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
+        { name = "supermaven" },
         { name = "buffer" },
+        { name = "nvim_lsp" },
         { name = "path" },
       },
+
       formatting = {
         format = function(entry, vim_item)
-          vim_item.kind = require("lspkind").presets.default[vim_item.kind]
+          if entry.source.name == "path" then
+            local icon, icon_color = devicons.get_icon(vim_item.kind)
+            if icon then
+              vim_item.kind = icon
+            end
+          else
+            vim_item.kind = kind_icons[vim_item.kind] or vim_item.kind
+          end
+
           vim_item.menu = ({
-            nvim_lsp = "[LSP]",
             buffer = "[Buffer]",
-            nvim_lua = "[Lua]",
+            nvim_lsp = "[LSP]",
             path = "[Path]",
+            supermaven = "[AI]",
           })[entry.source.name]
+
           return vim_item
         end,
       },
