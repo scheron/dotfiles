ec5895e0acedbcc7bbdfbfe3844eb14468ccfaed
Oleg 👾 <infectedbyjs@gmail.com>
2025-05-21 21:10:54 +1000

    update

--- nvim/lua/plugins/overrides/telescope.lua
+++ nvim/lua/plugins/overrides/telescope.lua
@@ -1,20 +1,39 @@
-require("telescope")
-  .setup({
-    defaults = {
-      -- layout_strategy = "horizontal",
-      sorting_strategy = "ascending",
-      prompt_prefix = " ",
-      selection_caret = " ",
-      path_display = { "truncate" },
-      file_ignore_patterns = { "node_modules", ".git" }, -- Ignore specific folders
+require("telescope").setup {
+  defaults = {
+    sorting_strategy = "ascending",
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "truncate" },
+    file_ignore_patterns = { "node_modules", ".git" },
+    mappings = {
+      i = {
+        ["<CR>"] = function(prompt_bufnr)
+          require("telescope.actions").close(prompt_bufnr)
+          require("telescope.actions").select_default(prompt_bufnr)
+        end,
+      },
     },
-    pickers = {
-      find_files = { theme = "dropdown" },
-      buffers = { theme = "dropdown" },
-      live_grep = { theme = "dropdown" },
-      grep_string = { theme = "dropdown" },
-      git_files = { theme = "dropdown" },
-      todos = { theme = "dropdown" },
+  },
+  pickers = {
+    find_files = { theme = "dropdown" },
+    buffers = { theme = "dropdown" },
+    live_grep = { theme = "dropdown" },
+    grep_string = { theme = "dropdown" },
+    git_files = { theme = "dropdown" },
+    todos = { theme = "dropdown" },
+    lsp_definitions = { -- Настройка для `gd` (Go to Definition)
+      theme = "dropdown",
+      attach_mappings = function(_, map)
+        map("i", "<CR>", function(prompt_bufnr)
+          require("telescope.actions").close(prompt_bufnr)
+          require("telescope.actions").select_default(prompt_bufnr)
+        end)
+        return true
+      end,
     },
-  })
-  -- require("telescope").load_extension "noice"
+  },
+}
+
+vim.keymap.set("n", "gd", function()
+  require("telescope.builtin").lsp_definitions()
+end, { desc = "Go to Definition (Telescope, auto-close)" })
